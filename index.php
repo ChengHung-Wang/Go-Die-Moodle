51c39af7bd7edf322581ee89e6ada870f9e9ca32
ChengHung-Wang <apple1314520iphone@icloud.com>
2022-05-10 00:23:01 +0800

    font-family

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
     <title>FuckMoodle Build Message</title>
     <style>
         body {
+            font-family: Helvetica Neue,Helvetica,PingFang SC,Hiragino Sans GB,Microsoft YaHei,SimSun,sans-serif;
             margin: 0;
             padding: 0;
             width: 100vw;
